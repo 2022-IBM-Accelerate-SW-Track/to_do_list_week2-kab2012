4bacae7e5deeed1da856aa2e7640e2bdc62fc9f5
kab2012 <jade43434@gmail.com>
2022-06-15 20:06:39 -0400

    add date to tasks

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,40 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import "./About.css";
 import profile_pic from "../assets/profile_pic.jpg";
-
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
+        <div>
         <div class="split left">
           <div className="centered">
-            <img
+            <img 
               className="profile_image"
               src={profile_pic}
               alt="Profile Pic"
-            ></img>
+              ></img>
           </div>
         </div>
         <div className="split right">
           <div className="centered">
-            <div className="name_title">Your Name</div>
+            <div className="name_title">Kimberly Brown</div>
             <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+              <div className="category_heading">Interests</div>
+                <div className="category_text">
+                  <ul><li>Reading</li><li>Walking</li><li>Cooking</li></ul>
+                  </div>
+              <div className="category_heading">Fun Facts</div>
+                <div className="category_text">
+                    <ul><li>I used to have a pet fish.</li>
+                    <li>I love to visit the mountains of NC.</li>
+                    <li>I enjoy learning about history.</li></ul>
+                    </div>
             </div>
           </div>
         </div>
+      </div> 
       </div>
-    );
+    )
   }
 }
